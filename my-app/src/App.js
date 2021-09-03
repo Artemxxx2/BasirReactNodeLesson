c6a3d55c19fac1ddb2faa47f2001873438ca65c0
Artemxxx2 <akolobnevstudio@gmail.com>
2021-09-03 09:55:31 +0300

    ReactAppWasCreated

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import data from './data.js'
+function App() {
+ let productData = data.products.map(el =>{
+   debugger
+    return   <div className="row center">
+    <div className="card">
+      <a href="product.html">
+       <img src={el.img} alt="" />
+      </a>
+      <div className="card-body">
+        <a href="product.html">
+          <h2>{el.name}</h2>
+        </a>
+        <div className="rating">
+          <span><i className="fa fa-star"></i></span>
+          <span><i className="fa fa-star"></i></span>
+          <span><i className="fa fa-star"></i></span>
+          <span><i className="fa fa-star"></i></span>
+          <span><i className="fa fa-star"></i></span>
+        </div>
+        <div className="price">{el.price}$</div>
+      </div>
+    </div>
+  </div>
+         
+  })
+  return (
+    <div className="grid-container">
+    <header className="center">
+      <div><a href="index.html" className="amazona">amazona</a></div>
+      <div>
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign in</a>
+      </div>
+    </header>
+    <main>
+  {productData}
+    </main>
+    <footer className="footer center">All rights reserved</footer>
+  </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/data.js
@@ -0,0 +1,35 @@
+const data ={
+    products:[
+        {
+            id:1,
+            price:120,
+            name:'Nike Slim Shirts',
+            img:null
+        },
+        {
+            id:2,
+            price:88,
+            name:'Addidas shoes',
+            img:null
+        },
+        {
+            id:3,
+            price:150,
+            name:'sunglasses',
+            img:null
+        },
+        {
+            id:4,
+            price:1,
+            name:'Smth cheap :)',
+            img:null
+        },
+        {
+            id:5,
+            price:135,
+            name:'Lacoste shirt',
+            img:null
+        },
+    ]
+}
+export default data
